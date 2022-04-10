8de130731f021af10727a14108025aa22794fa2a
Alex Conder <alex.v.conder@gmail.com>
2022-04-10 14:34:23 -0300

    Layout completo

--- flex/src/components/Header/index.js
+++ flex/src/components/Header/index.js
@@ -4,8 +4,8 @@ import './styles.css';
 const Header = () => {
   return (
     <header className="Header">
-      <div>
-        <div>
+      <div className="container">
+        <div className="Header-flexGrid">
           <h2>FlexBlog</h2>
           <button>Seja feliz!</button>
         </div>
--- flex/src/components/Main/index.js
+++ flex/src/components/Main/index.js
@@ -5,7 +5,7 @@ import './styles.css';
 const Main = () => {
   return (
     <div className='Main'>
-        <div>
+        <div className="container small">
             <h2>Posts em Destaque</h2>
             <CardList />
         </div>   
